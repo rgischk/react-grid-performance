7da8a156d25bc76afe1b96ddf8ea3dd30af0996c
Roman Gischkowski <gischkowski@encoway.de>
2022-06-16 13:06:10 +0200

    Add font files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import './App.css';
 import {Button, ButtonGroup, FormControlLabel, FormGroup, Switch} from "@mui/material";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {MuiXDataGrid} from "./tables/MuiXDataGrid";
 import {DevExtremeReactiveGrid} from "./tables/DevExtremeReactiveGrid";
 import {DevExtremeXDataGrid} from "./tables/DevExtremeXDataGrid";
